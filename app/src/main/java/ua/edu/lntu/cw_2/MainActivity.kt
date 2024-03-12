db340cd06857862b1e36cc2176dbbf9595261a47
ArtemGoloviy <58403271+ArtemGoloviy@users.noreply.github.com>
2024-03-12 14:42:53 +0200

    нова функція увійти

---
+++ app/src/main/java/ua/edu/lntu/cw_2/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.ipz_cw_2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ua.edu.lntu.cw_2.ui.theme.IPZ_CW_2_Holovii_ArtemTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            IPZ_CW_2_Holovii_ArtemTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    SignIn(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SignIn(modifier: Modifier = Modifier) {}
